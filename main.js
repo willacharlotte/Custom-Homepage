5bf09cee758a6034b49e4a2628fbcf1ac124b49e
willyle021 <willyle021@student.wethinkcode.co.za>
2022-11-16 12:09:32 +0200

    Removed webmanifest, changed main.js script tag to be type text/javascript and added fetchDailyPuzzle function to main.js

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-const initBoard = (position='start', orientation='white') => {
+const defaultPuzzle = {
+    "title": "Error loading daily puzzle",
+    "url": null,
+    "publish_time": null,
+    "fen": "start",
+    "pgn": null,
+    "image": null
+}
+
+async function fetchDailyPuzzle() {
+    return fetch("https://api.chess.com/pub/puzzle")
+        .then(response => {
+            if (response.ok){
+                return response.json();
+            } else {
+                return defaultPuzzle;
+            }
+        });
+}
+
+
+const initStaticBoard = (position='start', orientation='white') => {
     const config = {
         draggable: true,
         position: position,
@@ -9,4 +30,4 @@ const initBoard = (position='start', orientation='white') => {
     return Chessboard('chessboard', config);
 }
 
-const board = initBoard();
+const board = initStaticBoard();
