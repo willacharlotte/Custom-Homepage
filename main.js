a4675f619fb505af62e1eb1d0eca013f2638e9e2
willyle021 <willyle021@student.wethinkcode.co.za>
2022-11-16 08:39:42 +0200

    Renamed chessboard-style-overwrite to chessboard-style. Moved chess divs from style to chessboard-style. Added geometric pieces.

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const initBoard = (position='start', orientation='white') => {
         position: position,
         orientation: orientation,
         showNotation: false,
-        pieceTheme: 'img/chesspieces/wikipedia/{piece}.png'
+        pieceTheme: 'img/chesspieces/geometric/{piece}.png'
     };
     return Chessboard('chessboard', config);
 }
