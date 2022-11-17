15e1988091da822c89811cbebb8241bc4e8dbaa0
willyle021 <willyle021@student.wethinkcode.co.za>
2022-11-17 14:29:42 +0200

    Added grow/shrink functionality to chessboard

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const defaultPuzzle = {
 async function fetchDailyPuzzle() {
     return fetch("https://api.chess.com/pub/puzzle")
         .then(response => {
-            if (response.ok){
+            if (response.ok) {
                 return response.json();
             } else {
                 return defaultPuzzle;
@@ -18,16 +18,63 @@ async function fetchDailyPuzzle() {
         });
 }
 
-
-const initStaticBoard = (position='start', orientation='white') => {
+const initBoard = (draggable, position = 'start', orientation = 'white') => {
     const config = {
-        draggable: true,
+        draggable: draggable,
         position: position,
         orientation: orientation,
         showNotation: false,
         pieceTheme: 'img/chesspieces/geometric/{piece}.png'
     };
     return Chessboard('chessboard', config);
+};
+
+const miniToMaxi = (element) => {
+    element.removeClass('mini-board');
+    element.addClass('maxi-board');
 }
 
-const board = initStaticBoard();
+const maxiToMini = (element) => {
+    element.removeClass('maxi-board');
+    element.addClass('mini-board');
+}
+
+const initMiniBoard = (position = 'start', orientation = 'white') => {
+    const board = initBoard(false, position, orientation);
+    maxiToMini($('#chessboard'));
+    maxiToMini($('.board-b72b1'));
+    return board;
+}
+
+const initMaxiBoard = (position = 'start', orientation = 'white') => {
+    const board = initBoard(true, position, orientation);
+    miniToMaxi($('#chessboard'));
+    miniToMaxi($('.board-b72b1'));
+    return board;
+}
+
+let boardObj;
+
+const minimiseBoard = () => {
+    boardObj = initMiniBoard();
+
+    maxiToMini($('#minimise-board'));
+    maxiToMini($('#header-space'));
+
+    $('.board-b72b1.mini-board').click(maximiseBoard);
+
+    boardObj.resize();
+}
+
+const maximiseBoard = () => {
+    boardObj = initMaxiBoard();
+
+    miniToMaxi($('#minimise-board'));
+    miniToMaxi($('#header-space'));
+
+    $('#minimise-cross').click(minimiseBoard);
+
+    boardObj.resize();
+}
+
+minimiseBoard();
